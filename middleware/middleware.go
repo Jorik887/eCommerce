14c0722e6d5b56248d51e1504e0ff4f9767957ae
Jorik887 <baskahaska0708@mail.ru>
2024-05-05 17:35:55 +0300

    first commit

---
+++ middleware/middleware.go
@@ -0,0 +1,29 @@
+package middleware
+
+import (
+	"net/http"
+
+	token "github.com/Jorik887/eCommerce/tokens"
+	"github.com/gin-gonic/gin"
+)
+
+func Authentification() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ClientToken := c.Request.Header.Get("token")
+		if ClientToken == "" {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "No authorization error provided"})
+			c.Abort()
+			return
+		}
+		claims, err := token.ValidateToken(ClientToken)
+		if err != "" {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+			c.Abort()
+			return
+		}
+
+		c.Set("email", claims.Email)
+		c.Set("uid", claims.Uid)
+		c.Next()
+	}
+}
